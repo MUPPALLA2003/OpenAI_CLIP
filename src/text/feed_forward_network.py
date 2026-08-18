a350c1ff410227e83337d93d42551417ece292be
MUPPALLA2003 <muppallakomaliitkgp2022@kgpian.iitkgp.ac.in>
2026-08-18 21:14:12 +0530

    Feed Forward Network

---
+++ src/text/feed_forward_network.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class FeedForwardNetwork(nn.Module):
+
+    def __init__(self,in_features:int,hidden_features:int,out_features:int,mlp_p:float = 0.0) -> None:
+
+        super().__init__()
+
+        self.in_features = in_features
+        self.fc1 = nn.Linear(in_features,hidden_features)
+        self.act = nn.GELU(approximate='tanh')
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(mlp_p)
+        
+    def forward(self,x:torch.Tensor) -> torch.Tensor:
+        
+        if x.shape[-1] != self.in_features:
+
+            raise ValueError(f"Expected last dim {self.in_features}, got {x.shape[-1]}")
+        
+        x = self.act(self.fc1(x))  
+        x = self.drop(x)
+        x = self.fc2(x)             
+        x = self.drop(x)
+                
+        return x
